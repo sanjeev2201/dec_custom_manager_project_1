ee23a5f1474f71437f4f84d92187d9390b81ec56
sanjeev2201 <sanjeevkumarrai132@gmail.com>
2022-12-23 13:29:18 +0530

    new url added

--- MyDB_PROJECT/myapp/urls.py
+++ MyDB_PROJECT/myapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .  import views
 urlpatterns = [
+    path('home/',views.home_page,name='home'),
     path('student/', views.Student_index,name='student'),
-    path('student_proxy/',views.Student_proxy_index,name='student-proxy')
+    path('student-list/',views.Student_list,name='student-list'),
+    path('student_proxy/',views.Student_proxy_index,name='student-proxy'),
+    path('student-proxy-list/',views.Student_proxy_list,name='student-proxy-list'),
+    path('edit_proxy/<int:pk>', views.update_proxy_student,name='update-proxy-student'),
+    path('delete_proxy/<int:pk>', views.delete,name='delete-proxy-student'),
 ]
